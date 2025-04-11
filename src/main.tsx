fb67439affc5d22537a90f33d6cb486db594d326
Lucas Amaral <lucas.trevvos@gmail.com>
2025-04-10 22:15:14 -0300

    Adiciona HelmetProvider para suporte ao react-helmet-async

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,16 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router'
+import { HelmetProvider } from 'react-helmet-async'
 import { AppRoutes } from './routes/AppRoutes'
 import './main.css'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <BrowserRouter>
-      <AppRoutes />
-    </BrowserRouter>
-  </StrictMode>,
+    <HelmetProvider>
+      <BrowserRouter>
+        <AppRoutes />
+      </BrowserRouter>
+    </HelmetProvider>
+  </StrictMode>
 )
